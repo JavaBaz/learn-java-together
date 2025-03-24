5e6493b641a9b8833842dc480f9ef571566e12b5
Mehrana <fatemeh.safaeiiiii@gmail.com>
2025-03-24 21:31:55 +0330

    Add Calculator48

---
+++ week01/src/main/java/com/github/javabaz/person48/Calculator48.java
@@ -0,0 +1,21 @@
+package com.github.javabaz.person48;
+
+public class Calculator48 {
+
+    public int printSum(int a, int b) {
+        int sum = a + b;
+        return sum;
+    }
+    public int printSubtract(int a, int b) {
+        int subtract = a - b;
+        return subtract;
+    }
+    public int printMultiply(int a, int b) {
+        int multiply = a * b;
+        return multiply;
+    }
+    public float printDivide(float a, float b) {
+        float divide = a / b;
+        return divide;
+    }
+}
